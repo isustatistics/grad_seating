4feb1d1eff5a8431788252c21150412078cbbe79
Jason Saporta <jasons89@gmail.com>
2021-11-05 13:22:46 -0500

    Removed stale comments, added makefile for ease of use

--- code/pod-charts.py
+++ code/pod-charts.py
@@ -1,11 +1,8 @@
 # Setup
-import pathlib
-
 import pandas as pd
 
-data_dir = pathlib.Path("data")
-students = pd.read_csv(data_dir / "fake_data.csv")
-layouts = pd.read_csv(data_dir / "layouts.csv")
+students = pd.read_csv("data/fake_data.csv")
+layouts = pd.read_csv("data/layouts.csv")
 
 # Template loading and image manipulation
 def get_template(layout):
@@ -17,23 +14,17 @@ def get_template(layout):
             return f.read()
 
 # Main loop: Iterate over pods and create a chart from the appropriate template for each one
-# If there's less than 5 students in a pod, this will leave filler text in the chart
 for room in students["room"].unique():
     in_room = students.loc[students["room"] == room]
     # Only pods for now
     if len(in_room) != 5:
         continue
     layout = layouts.loc[layouts["room"] == room, "layout"].iloc[0]
-    print(layout)
     chart = get_template(layout)
 
     for student in in_room.itertuples():
-        seat = student.seat
-        first = student.first
-        last = student.last
-
-        chart = chart.replace(f"first{seat}", first)
-        chart = chart.replace(f"last{seat}", last)
+        chart = chart.replace(f"first{student.seat}", student.first)
+        chart = chart.replace(f"last{student.seat}", student.last)
 
     with open(f"outputs/room_{room}_chart.svg", "w") as f:
         f.write(chart)
