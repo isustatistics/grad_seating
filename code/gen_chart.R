05c0724c866f5d1a8e8536eabbd020f1231e040e
Josh Berlinski <jberlin@iastate.edu>
2022-08-09 11:21:20 -0500

    added nicknames and office numbers to charts

--- code/gen_chart.R
+++ code/gen_chart.R
@@ -22,6 +22,8 @@ tex_preamble <- "\\documentclass[11pt]{article}
 
 tex_post <- "\\end{tikzpicture}
 \\end{figure}
+\\vfill
+officenumber
 \\end{document}"
 
 tex_even <- list(
@@ -60,19 +62,26 @@ gen_charts <- function(seats) {
 
         # this creates the node for each of the individuals
         indiv_nodes <- apply(office, 1, function(x) {
+            lay <- as.integer(x[7])
             fname <- x[4]
             lname <- x[5]
+            nname <- x[6]
             seat <- as.integer(x[3])
             # do not ask why you need to check equality here
-            template <- ifelse(x[6] == 1, tex_odd[[seat]], tex_even[[seat]])
-            return(sub("replace", paste(fname, "\\\\\\\\", lname), template))
+            template <- ifelse(lay == 1, tex_odd[[seat]], tex_even[[seat]])
+            return(sub("replace", paste0(fname, " \`\`", nname, "\"", "\\\\\\\\", lname), template))
           }
         )
 
+        onum <- office$room[1]
+
         # paste them all together and make the tex file
-        interior  <- paste(Reduce(c, indiv_nodes), sep = "\n")
+        # interior  <- paste(Reduce(c, indiv_nodes), sep = "\n")
         interior  <- paste(indiv_nodes, collapse = "\n")
-        full_file <- paste(tex_preamble, interior, tex_post, sep = "\n")
+        # add office number to bottom
+        this_post <- sub("officenumber", onum, tex_post)
+        # interior <- paste(interior, onum, sep = "\n\\hfill\\vfill")
+        full_file <- paste(tex_preamble, interior, this_post, sep = "\n")
 
         list(fname = paste0("../tex/", office$building, office$room, "_seatchart.tex"),
              content = full_file)
